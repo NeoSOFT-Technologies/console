28bf8ba512151d7dca37154b18ca7163d80e31dc
paras-1999 <paras.saxena@neosoftmail.com>
2022-01-25 12:44:57 +0530

    initial setup

---
+++ demo_1/src/app/shared/Footer.js
@@ -0,0 +1,37 @@
+import React, { Component } from "react";
+
+class Footer extends Component {
+  render() {
+    return (
+      <footer className="footer">
+        <div className="d-sm-flex justify-content-center justify-content-sm-between py-2">
+          <span className="text-muted text-center text-sm-left d-block d-sm-inline-block">
+            Copyright ©{" "}
+            <a
+              href="https://www.bootstrapdash.com/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              bootstrapdash.com{" "}
+            </a>
+            2020
+          </span>
+          <span className="float-none float-sm-right d-block mt-1 mt-sm-0 text-center">
+            Free{" "}
+            <a
+              href="https://www.bootstrapdash.com/react-admin-templates/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {" "}
+              react admin{" "}
+            </a>{" "}
+            templates from BootstrapDash.com.{" "}
+          </span>
+        </div>
+      </footer>
+    );
+  }
+}
+
+export default Footer;
