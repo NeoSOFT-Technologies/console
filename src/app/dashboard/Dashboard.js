929cd10338d71b28a036ddee0dbfbd0c43514677
jeffNeosoft <jeff.thaiparambil@neosoftmail.com>
2022-02-15 17:49:28 +0530

    changes in dashboard

--- src/app/dashboard/Dashboard.js
+++ src/app/dashboard/Dashboard.js
@@ -1,60 +1,41 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { Container, Card } from "react-bootstrap";
-import axios from "axios";
+import { useSelector } from "react-redux";
 export default function Dashboard() {
-  const [details, setDetails] = useState([]);
-  useEffect(() => {
-    let url = " http://localhost:3001/Registration";
-    axios.get(url).then((res) => {
-      setDetails(res.data);
-    });
-  }, []);
-  const email = sessionStorage.getItem("user");
-  console.log(email);
-
-  console.log(details);
-  //console.log(sessionStorage.getItem("user"));
+  const user = useSelector((state) => state.setUserData);
   return (
     <>
       <Container>
-        {details &&
-          details.map((val) => {
-            if (val.email === email) {
-              return (
-                <Card style={{ width: "550px" }} className="m-1 p-2">
-                  <Card.Title
-                    style={{ fontSize: "32px" }}
-                    className="text-center"
-                  >
-                    Tenant Details
-                  </Card.Title>
-                  <hr />
-                  <Card.Body
-                    style={{
-                      fontSize: "20px",
-                      fontWeight: "bold",
-                      textAlign: "left",
-                    }}
-                  >
-                    <Card.Text>
-                      Name:&nbsp; <span>{val.name} </span>
-                    </Card.Text>
-                    <Card.Text>
-                      <span>Description:&nbsp;{val.description}</span>
-                    </Card.Text>
-                    <Card.Text>
-                      <span>UserId:&nbsp;{val.userid}</span>
-                    </Card.Text>
-                    <Card.Text>
-                      <span> Email:&nbsp;{val.email}</span>
-                    </Card.Text>
+        {user && (
+          <Card style={{ width: "550px" }} className="m-1 p-2">
+            <Card.Title style={{ fontSize: "32px" }} className="text-center">
+              Tenant Details
+            </Card.Title>
+            <hr />
+            <Card.Body
+              style={{
+                fontSize: "20px",
+                fontWeight: "bold",
+                textAlign: "left",
+              }}
+            >
+              <Card.Text>
+                Name:&nbsp; <span>{user.name} </span>
+              </Card.Text>
+              <Card.Text>
+                <span>Description:&nbsp;{user.description}</span>
+              </Card.Text>
+              <Card.Text>
+                <span>UserId:&nbsp;{user.userid}</span>
+              </Card.Text>
+              <Card.Text>
+                <span> Email:&nbsp;{user.email}</span>
+              </Card.Text>
 
-                    <hr />
-                  </Card.Body>
-                </Card>
-              );
-            }
-          })}
+              <hr />
+            </Card.Body>
+          </Card>
+        )}
       </Container>
     </>
   );
