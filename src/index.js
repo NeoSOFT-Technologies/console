6910a95e0635b3a2476f81f800cf7884e9aac0a9
rahulkenchi <rahul.kenchi@neosoftmail.com>
2022-02-18 10:50:00 +0530

    merged

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./app/App";
 import { Provider } from "react-redux";
 import store from "./app/redux/store/store";
+import { makeServer } from "./Miragejs/index";
+
+makeServer({ environment: "test " });
 // import "./i18n";
 ReactDOM.render(
   <BrowserRouter>
