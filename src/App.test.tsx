53968042fb833b2b0d132cd3a1fdb7e82492f523
rahulkenchi <rahul.kenchi@neosoftmail.com>
2022-03-11 19:10:36 +0530

    fixed App.test.tsx

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,14 +1,17 @@
 import React from "react";
 import { render } from "@testing-library/react";
 import { Provider } from "react-redux";
+import { BrowserRouter } from 'react-router-dom'
 import store from "./store";
 import App from "./App";
 
 test("renders learn react link", () => {
   const { container } = render(
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
   );
   container.querySelector(".landing");
   expect(container.firstChild).toMatchSnapshot();
