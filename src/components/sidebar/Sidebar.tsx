78f685b28e0dde77c08ca10b85d347831cbcba2e
rahulkenchi <rahul.kenchi@neosoftmail.com>
2022-04-19 20:08:27 +0530

    added subroutes

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,6 +1,7 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 // import { Button } from "react-bootstrap";
-import { Link, useLocation, useNavigate } from "react-router-dom";
+import Collapse from "react-bootstrap/esm/Collapse";
+import { Link, useLocation } from "react-router-dom";
 import withRouter from "../../WithRouter";
 import adminRoutes from "../../routes/tenants/admin";
 import tenantRoutes from "../../routes/tenants/tenants";
@@ -8,6 +9,7 @@ import userRoutes from "../../routes/tenants/user-routes";
 import { RootState } from "../../store";
 import { useAppSelector } from "../../store/hooks";
 import { IUserDataState } from "../../types";
+import "./Sidebar.scss";
 
 interface IConditions {
   data: string;
@@ -17,7 +19,7 @@ interface IConditions {
 
 export const Sidebar = () => {
   const location = useLocation();
-  const navigate = useNavigate();
+  // const navigate = useNavigate();
   const isPathActive = (path: string) => {
     return location.pathname.startsWith(path);
   };
@@ -28,66 +30,36 @@ export const Sidebar = () => {
     (state: RootState) => state.loginType
   );
   const [routes, setRoutes] = useState([{ path: "", title: "", icon: "" }]);
-  const [switchRoutes, setSwitchRoutes] = useState(false);
-  // useEffect(() => {
-  //   if (user.data && loginType.data === "admin") {
-  //     setRoutes(adminRoutes);
-  //   } else if (user.data && loginType.data === "tenant") {
-  //     setRoutes(tenantRoutes);
-  //   } else if (user.data && loginType.data === "user") {
-  //     setRoutes(userRoutes);
-  //   }
-  // }, [user.data]);
-  const setSubRoutes = (type: string) => {
-    switch (type) {
-      case "route": {
-        if (user.data && loginType.data === "admin") {
-          setRoutes(adminRoutes);
-          navigate("/admindashboard");
-        } else if (user.data && loginType.data === "tenant") {
-          setRoutes(tenantRoutes);
-          navigate("/tenantdashboard");
-        } else if (user.data && loginType.data === "user") {
-          setRoutes(userRoutes);
-          navigate("/userdashboard");
-        }
+  const [gatewayroutes, setGatewayRoutes] = useState([
+    { path: "", title: "", icon: "" },
+  ]);
+  const [saasroutes, setSaasRoutes] = useState([
+    { path: "", title: "", icon: "" },
+  ]);
 
-        break;
-      }
-      case "gateway": {
-        if (user.data && loginType.data === "admin") {
-          setRoutes(adminRoutes);
-          navigate("/admindashboard");
-        } else if (user.data && loginType.data === "tenant") {
-          setRoutes(tenantRoutes);
-          navigate("/tenantdashboard");
-        } else if (user.data && loginType.data === "user") {
-          setRoutes(userRoutes);
-          navigate("/userdashboard");
-        }
-
-        break;
-      }
-      case "saas": {
-        if (user.data && loginType.data === "admin") {
-          setRoutes(adminRoutes);
-          navigate("/admindashboard");
-        } else if (user.data && loginType.data === "tenant") {
-          setRoutes(tenantRoutes);
-          navigate("/tenantdashboard");
-        } else if (user.data && loginType.data === "user") {
-          setRoutes(userRoutes);
-          navigate("/userdashboard");
-        }
-
-        break;
-      }
-      default: {
-        navigate("/error-pages/error-404");
-      }
+  // const [switchRoutes, setSwitchRoutes] = useState(false);
+  useEffect(() => {
+    if (user.data && loginType.data === "admin") {
+      setRoutes(adminRoutes);
+      setGatewayRoutes(adminRoutes);
+      setSaasRoutes(adminRoutes);
+    } else if (user.data && loginType.data === "tenant") {
+      setRoutes(tenantRoutes);
+      setGatewayRoutes(adminRoutes);
+      setSaasRoutes(adminRoutes);
+    } else if (user.data && loginType.data === "user") {
+      setRoutes(userRoutes);
+      setGatewayRoutes(adminRoutes);
+      setSaasRoutes(adminRoutes);
     }
-    setSwitchRoutes(true);
-  };
+  }, [user.data]);
+
+  const [subMenu, setSubMenu] = useState({
+    logger: false,
+    gateway: false,
+    saas: false,
+  });
+
   return (
     <>
       <nav className="sidebar sidebar-offcanvas" id="sidebar">
@@ -118,58 +90,132 @@ export const Sidebar = () => {
               <i className="bi bi-bookmark-star-fill text-success nav-profile-badge"></i>
             </a>
           </li>
-          {switchRoutes ? (
-            <>
-              {routes.map((route, index) => (
-                <li
-                  key={`route${index}`}
-                  className={
-                    isPathActive(route.path) ? "nav-item active" : "nav-item"
+          <li className="nav-item">
+            <Link className="nav-link" to="#">
+              <div className="d-flex justify-content-between w-100 ">
+                <span
+                  className="menu-title lh-2"
+                  onClick={() =>
+                    setSubMenu({ ...subMenu, logger: !subMenu.logger })
                   }
                 >
-                  <Link className="nav-link" to={route.path}>
-                    <span className="menu-title">
-                      <>{route.title}</>
-                    </span>
-                    <i className={route.title}></i>
-                  </Link>
-                </li>
-              ))}
-            </>
-          ) : (
-            <>
-              <ul className="nav">
-                <li
-                  className="nav-item"
-                  onClick={() => setSubRoutes("route")}
-                  data-testid="logintype"
-                >
-                  <span className="nav-link">
-                    <span className="menu-title ">{loginType.data}</span>
-                  </span>
-                </li>
-
-                <li
-                  className="nav-item"
-                  data-testid="gateway-item"
-                  onClick={() => setSubRoutes("gateway")}
-                >
-                  <span className="nav-link">
-                    <span className="menu-title">Gateway</span>
-                  </span>
-                </li>
-                <li
-                  className="nav-item"
-                  onClick={() => setSubRoutes("saas")}
-                  data-testid="sass"
-                >
-                  <span className="nav-link">
-                    <span className="menu-title ">SaaS</span>
-                  </span>
-                </li>
+                  {loginType.data}
+                </span>
+                <i
+                  className={`bi bi-caret-left ${
+                    subMenu.logger ? "r-90" : "r90"
+                  }`}
+                ></i>
+              </div>
+            </Link>
+            <Collapse in={subMenu.logger}>
+              <ul className="nav flex-column  list-unstyled p-0">
+                {routes.map((route, index) => (
+                  <li
+                    key={`route${index}`}
+                    className={
+                      isPathActive(route.path)
+                        ? "nav-item active px-3"
+                        : "nav-item px-3"
+                    }
+                  >
+                    <Link className="nav-link pt-0" to={route.path}>
+                      <div className="d-flex justify-content-between w-100 ">
+                        <span className="menu-title lh-2">
+                          <>{route.title}</>
+                        </span>
+                        <i className={route.icon}></i>
+                      </div>
+                    </Link>
+                  </li>
+                ))}
               </ul>
-            </>
-          )}
+            </Collapse>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="#">
+              <div className="d-flex justify-content-between w-100 ">
+                <span
+                  className="menu-title lh-2"
+                  onClick={() =>
+                    setSubMenu({ ...subMenu, gateway: !subMenu.gateway })
+                  }
+                >
+                  Gateway
+                </span>
+                <i
+                  className={`bi bi-caret-left ${
+                    subMenu.gateway ? "r-90" : "r90"
+                  }`}
+                ></i>
+              </div>
+            </Link>
+            <Collapse in={subMenu.gateway}>
+              <ul className="nav flex-column  list-unstyled p-0">
+                {gatewayroutes.map((route, index) => (
+                  <li
+                    key={`route${index}`}
+                    className={
+                      isPathActive(route.path)
+                        ? "nav-item active px-3"
+                        : "nav-item px-3"
+                    }
+                  >
+                    <Link className="nav-link pt-0" to={route.path}>
+                      <div className="d-flex justify-content-between w-100 ">
+                        <span className="menu-title lh-2">
+                          <>{route.title}</>
+                        </span>
+                        <i className={route.icon}></i>
+                      </div>
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            </Collapse>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="#">
+              <div className="d-flex justify-content-between w-100 ">
+                <span
+                  className="menu-title lh-2"
+                  onClick={() =>
+                    setSubMenu({ ...subMenu, saas: !subMenu.saas })
+                  }
+                >
+                  Saas
+                </span>
+                <i
+                  className={`bi bi-caret-left ${
+                    subMenu.saas ? "r-90" : "r90"
+                  }`}
+                ></i>
+              </div>
+            </Link>
+            <Collapse in={subMenu.saas}>
+              <ul className="nav flex-column  list-unstyled p-0">
+                {saasroutes.map((route, index) => (
+                  <li
+                    key={`route${index}`}
+                    className={
+                      isPathActive(route.path)
+                        ? "nav-item active px-3"
+                        : "nav-item px-3"
+                    }
+                  >
+                    <Link className="nav-link pt-0" to={route.path}>
+                      <div className="d-flex justify-content-between w-100 ">
+                        <span className="menu-title lh-2">
+                          <>{route.title}</>
+                        </span>
+                        <i className={route.icon}></i>
+                      </div>
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            </Collapse>
+          </li>
         </ul>
       </nav>
     </>
