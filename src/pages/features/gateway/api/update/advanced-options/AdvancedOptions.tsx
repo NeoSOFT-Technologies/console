86928f0f006b590ac960162e2feb99fb049364a4
dwivedi-ankita <ankita.dwivedi@neosoftmail.com>
2022-05-05 18:48:55 +0530

    Added Whitelisted and Blacklisted IPs components added

---
+++ src/pages/features/gateway/api/update/advanced-options/AdvancedOptions.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import BlacklistedIPs from "./blacklisted-ips/BlacklistedIPs";
+// import CorsOptions from "./cors-options/CorsOptions";
+import WhitelistedIPs from "./whitelisted-ips/WhitelistedIPs";
+
+export default function AdvancedOptions() {
+  return (
+    <div>
+      {/* <CorsOptions /> */}
+      <WhitelistedIPs />
+      <BlacklistedIPs />
+    </div>
+  );
+}
