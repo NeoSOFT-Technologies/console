d7df75f263cb85791f89e7f552674824af0eeb9d
Karthik Pillai <52003038+karthik261099@users.noreply.github.com>
2022-05-23 18:19:39 +0530

    Saas- Test case warnings resolved

---
+++ src/pages/features/saas/insert-data/InsertData.test.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { render, screen } from "@testing-library/react";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import store from "../../../../store/index";
+import InsertData from "./InsertData";
+
+describe("SAAS - INSERT DATA Component", () => {
+  it("Check if H1 rendered", () => {
+    render(
+      <BrowserRouter>
+        <Provider store={store}>
+          <InsertData />
+        </Provider>
+      </BrowserRouter>
+    );
+
+    const insertDataH1Tag = screen.getByText("Insert Data", {
+      exact: false,
+    });
+    expect(insertDataH1Tag).toBeInTheDocument();
+  });
+
+  it("Check if Save button rendered", () => {
+    render(
+      <BrowserRouter>
+        <Provider store={store}>
+          <InsertData />
+        </Provider>
+      </BrowserRouter>
+    );
+
+    const saveButtonElement = screen.getByRole("button", {
+      name: /save/i,
+    });
+    expect(saveButtonElement).toBeInTheDocument();
+  });
+});
