7f5428b8c4ed02e1118eb905ae415c37deedc1d0
mangeshdase <dasemangesh14@gmail.com>
2022-05-30 16:56:14 +0530

    edit table

--- src/pages/features/saas/edit-table/EditTable.tsx
+++ src/pages/features/saas/edit-table/EditTable.tsx
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from "react";
-import { Button, Col, Modal, Row, Table } from "react-bootstrap";
+import { Button, Col, InputGroup, Modal, Row, Table } from "react-bootstrap";
 import Form from "react-bootstrap/Form";
 import { useLocation } from "react-router-dom";
-import { getTables } from "../../../../store/features/saas/manage-table/get-tables/slice";
+import { getTableSchema } from "../../../../store/features/saas/manage-table/get-table-schema/slice";
 import { useAppDispatch, useAppSelector } from "../../../../store/hooks";
+import { ITableColumnData, ITableSchema } from "../../../../types/saas";
 import "./style.css";
 
 type LocationState = { tableName: string; tenantId: string };
@@ -12,33 +13,58 @@ export default function GetTables() {
   const location = useLocation();
 
   const { tableName, tenantId } = location.state as LocationState;
-  console.log(tableName);
-  console.log(tenantId);
-  const tableData = useAppSelector((v) => v.getTableSchemaState);
 
-  // const [tenantId] = useState("");
-  const getTableData: React.FormEventHandler<HTMLFormElement> = (
-    event: React.FormEvent
-  ) => {
-    event.preventDefault();
-    // console.log(tenantId);
-    dispatch(getTables(tenantId));
+  const tableData = useAppSelector((state) => state.getTableSchemaState);
+  console.log(tableData);
+
+  const obj: ITableSchema = {
+    tenantId: tenantId,
+    tableName: tableName,
   };
-  useEffect(() => {
-    // console.log(tableData);
-  }, [tableData.data, tableData.error]);
+  const [name, setName] = useState("");
+  const [type, setType] = useState("");
+  const [required, setRequired] = useState(Boolean);
+  const [partialSearch, setPartialSearch] = useState(Boolean);
+  const [filterable, setFilterable] = useState(Boolean);
+  const [sortable, setSortable] = useState(Boolean);
+  const [multiValue, setMultiValue] = useState(Boolean);
+  const [storable, setStorable] = useState(Boolean);
 
+  useEffect(() => {
+    dispatch(getTableSchema(obj));
+  }, []);
   const [show, setShow] = useState(false);
 
   const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
+  const handleShow = (columData: ITableColumnData) => {
+    setName(columData.name);
+    setType(columData.type);
+    setRequired(columData.required);
+    setPartialSearch(columData.partialSearch);
+    setFilterable(columData.filterable);
+    setSortable(columData.sortable);
+    setMultiValue(columData.multiValue);
+    setStorable(columData.storable);
+
+    setShow(true);
+  };
+  const obj2: ITableColumnData = {
+    name: name,
+    type: type,
+    required: required,
+    partialSearch: partialSearch,
+    filterable: filterable,
+    sortable: sortable,
+    multiValue: multiValue,
+    storable: storable,
+  };
 
   return (
     <div className="createbody">
       <h4 className="pl-5 pt-5">Edit Table</h4>
       <br></br>
 
-      <Form onSubmit={getTableData} className="pl-5">
+      <Form className="pl-5">
         <Row>
           <Col>
             <Row>
@@ -51,16 +77,13 @@ export default function GetTables() {
               </Form.Label>
 
               <Col sm lg="4">
-                <Form.Select
+                <InputGroup
                   aria-label="Default select example"
                   className="w-100 pr-3 pt-1 pb-1 createbody"
                   id="box"
                 >
-                  <option>Select User</option>
-                  <option value="1">S1</option>
-                  <option value="2">S2</option>
-                  <option value="3">S3</option>
-                </Form.Select>
+                  {tenantId}
+                </InputGroup>
               </Col>
             </Row>
             <br></br>
@@ -73,16 +96,13 @@ export default function GetTables() {
                 Table Name
               </Form.Label>
               <Col sm lg="4">
-                <Form.Select
+                <InputGroup
                   aria-label="Default select example"
                   className="w-100 pr-3 pt-1 pb-1 createbody"
                   id="box"
                 >
-                  <option>Select Table Name</option>
-                  <option value="1">One</option>
-                  <option value="2">Two</option>
-                  <option value="3">Three</option>
-                </Form.Select>
+                  {tableName}
+                </InputGroup>
               </Col>
             </Row>
             <br></br>
@@ -96,59 +116,50 @@ export default function GetTables() {
                 columns :
               </Form.Label>
               <Col sm lg="8" className="ml-2 mt-5 pl-1 pr-0 table-responsive ">
-                <Table bordered className="text-center pr-0">
-                  <thead>
-                    <tr id="test">
-                      <th>Name</th>
-                      <th>Title</th>
-                      <th>Required</th>
-                      <th>Partial Search</th>
-                      <th>Filterable</th>
-                      <th>Sortable</th>
-                      <th>Multivalue</th>
-                      <th>Storable</th>
-                      <th>Edit</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    <tr>
-                      <td>Ravi</td>
-                      <td>Product</td>
-                      <td>true</td>
-                      <td>false</td>
-                      <td>true </td>
-                      <td>false</td>
-                      <td>true</td>
-                      <td>true </td>
-                      <td>
-                        <i
-                          className="bi bi-pencil-square "
-                          data-toggle="modal"
-                          data-target="#exampleModalCenter"
-                          onClick={handleShow}
-                        ></i>{" "}
-                      </td>
-                    </tr>
-                    <tr>
-                      <td>Sam</td>
-                      <td>Price</td>
-                      <td>true</td>
-                      <td>false</td>
-                      <td>true </td>
-                      <td>false</td>
-                      <td>true</td>
-                      <td>true </td>
-                      <td>
-                        <i
-                          className="bi bi-pencil-square"
-                          data-toggle="modal"
-                          data-target="#exampleModalCenter"
-                          onClick={handleShow}
-                        ></i>{" "}
-                      </td>
-                    </tr>
-                  </tbody>
-                </Table>
+                {tableData.data !== undefined &&
+                tableData.data.columns.length > 0 ? (
+                  <Table bordered className="text-center pr-0">
+                    <thead>
+                      <tr id="test">
+                        <th>Name</th>
+                        <th>Type</th>
+                        <th>Required</th>
+                        <th>Partial Search</th>
+                        <th>Filterable</th>
+                        <th>Sortable</th>
+                        <th>Multivalue</th>
+                        <th>Storable</th>
+                        <th>Edit</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {tableData.data.columns.map((val) => (
+                        <>
+                          <tr>
+                            <td>{val.name}</td>
+                            <td>{val.type}</td>
+                            <td>{val.required.toString()}</td>
+                            <td>{val.partialSearch.toString()}</td>
+                            <td>{val.filterable.toString()}</td>
+                            <td>{val.sortable.toString()}</td>
+                            <td>{val.multiValue.toString()}</td>
+                            <td>{val.storable.toString()} </td>
+                            <td>
+                              <i
+                                className="bi bi-pencil-square "
+                                data-toggle="modal"
+                                data-target="#exampleModalCenter"
+                                onClick={() => handleShow(val)}
+                              ></i>
+                            </td>
+                          </tr>
+                        </>
+                      ))}
+                    </tbody>
+                  </Table>
+                ) : (
+                  <h2>No Data</h2>
+                )}
               </Col>
             </Row>
             <br />
@@ -171,7 +182,7 @@ export default function GetTables() {
         </div>
       </Row>
 
-      <Modal show={show} onHide={handleClose} size="lg">
+      <Modal show={show} data={obj2} onHide={handleClose} size="lg">
         <Modal.Header>
           <Modal.Title className="text-center">Add Column</Modal.Title>
           <button
@@ -194,8 +205,9 @@ export default function GetTables() {
                 <div className="input-group ">
                   <input
                     type="text"
-                    className="form-control text-center"
+                    className="form-control text-center read-only"
                     placeholder="Name"
+                    value={obj2.name}
                     aria-label="Name"
                     aria-describedby="basic-addon"
                   />
@@ -206,14 +218,15 @@ export default function GetTables() {
             <br></br>
             <Row>
               <Col sm lg="4">
-                <Form.Label className="ml-5 pt-2">Title</Form.Label>
+                <Form.Label className="ml-5 pt-2">Type</Form.Label>
               </Col>
 
               <Col sm lg="7">
                 <div className="input-group ">
                   <input
                     type="text"
-                    className="form-control text-center"
+                    className="form-control text-center read-only"
+                    value={obj2.type}
                     placeholder="Title"
                     aria-label="Title"
                     aria-describedby="basic-addon"
@@ -234,10 +247,18 @@ export default function GetTables() {
                   className="w-100 pr-3 pt-1 pb-1"
                   id="box"
                 >
-                  <option className="text-center">Select</option>
-                  <option value="1">S1</option>
-                  <option value="2">S2</option>
-                  <option value="3">S3</option>
+                  <option
+                    className="text-center"
+                    value={obj2.required.toString()}
+                  >
+                    {obj2.required.toString()}
+                  </option>
+                  <option
+                    className="text-center"
+                    value={(!obj2.required).toString()}
+                  >
+                    {(!obj2.required).toString()}
+                  </option>
                 </Form.Select>
               </Col>
             </Row>
@@ -254,10 +275,18 @@ export default function GetTables() {
                   className="w-100 pr-3 pt-1 pb-1"
                   id="box"
                 >
-                  <option className="text-center"> Select</option>
-                  <option value="1">S1</option>
-                  <option value="2">S2</option>
-                  <option value="3">S3</option>
+                  <option
+                    className="text-center"
+                    value={obj2.partialSearch.toString()}
+                  >
+                    {obj2.partialSearch.toString()}
+                  </option>
+                  <option
+                    className="text-center"
+                    value={(!obj2.partialSearch).toString()}
+                  >
+                    {(!obj2.partialSearch).toString()}
+                  </option>
                 </Form.Select>
               </Col>
             </Row>
@@ -274,10 +303,18 @@ export default function GetTables() {
                   className="w-100 pr-3 pt-1 pb-1"
                   id="box"
                 >
-                  <option className="text-center">Select</option>
-                  <option value="1">S1</option>
-                  <option value="2">S2</option>
-                  <option value="3">S3</option>
+                  <option
+                    className="text-center"
+                    value={obj2.filterable.toString()}
+                  >
+                    {obj2.filterable.toString()}
+                  </option>
+                  <option
+                    className="text-center"
+                    value={(!obj2.filterable).toString()}
+                  >
+                    {(!obj2.filterable).toString()}
+                  </option>
                 </Form.Select>
               </Col>
             </Row>
@@ -293,10 +330,18 @@ export default function GetTables() {
                   className="w-100 pr-3 pt-1 pb-1"
                   id="box"
                 >
-                  <option className="text-center">Select</option>
-                  <option value="1">S1</option>
-                  <option value="2">S2</option>
-                  <option value="3">S3</option>
+                  <option
+                    className="text-center"
+                    value={obj2.sortable.toString()}
+                  >
+                    {obj2.sortable.toString()}
+                  </option>
+                  <option
+                    className="text-center"
+                    value={(!obj2.sortable).toString()}
+                  >
+                    {(!obj2.sortable).toString()}
+                  </option>
                 </Form.Select>
               </Col>
             </Row>
@@ -312,10 +357,18 @@ export default function GetTables() {
                   className="w-100 pr-3 pt-1 pb-1"
                   id="box"
                 >
-                  <option className="text-center">Select</option>
-                  <option value="1">S1</option>
-                  <option value="2">S2</option>
-                  <option value="3">S3</option>
+                  <option
+                    className="text-center"
+                    value={obj2.multiValue.toString()}
+                  >
+                    {obj2.multiValue.toString()}
+                  </option>
+                  <option
+                    className="text-center"
+                    value={(!obj2.multiValue).toString()}
+                  >
+                    {(!obj2.multiValue).toString()}
+                  </option>
                 </Form.Select>
               </Col>
             </Row>
@@ -331,10 +384,18 @@ export default function GetTables() {
                   className="w-100 pr-3 pt-1 pb-1"
                   id="box"
                 >
-                  <option className="text-center"> select</option>
-                  <option value="1">S1</option>
-                  <option value="2">S2</option>
-                  <option value="3">S3</option>
+                  <option
+                    className="text-center"
+                    value={obj2.storable.toString()}
+                  >
+                    {obj2.storable.toString()}
+                  </option>
+                  <option
+                    className="text-center"
+                    value={(!obj2.storable).toString()}
+                  >
+                    {(!obj2.storable).toString()}
+                  </option>
                 </Form.Select>
               </Col>
             </Row>
