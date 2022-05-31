08ed7d26daa8bb2d4d3203d8aaa4635274508833
gurpreet1101 <mailtogurpreet1991@gmail.com>
2022-05-31 12:35:16 +0530

    changes in "Search with query filed" slice test file.

--- src/store/features/saas/search-data/with-query-field/slice.test.tsx
+++ src/store/features/saas/search-data/with-query-field/slice.test.tsx
@@ -3,28 +3,28 @@ import store from "../../../../index";
 import { searchDataWithQueryField } from "./slice";
 
 describe("SAAS - SEARCH Tables With Query Field Slice", () => {
-  // test("SAAS - SEARCH Tables With Query Field Success", async () => {
-  //   mockApi
-  //     .onGet(
-  //       "testTable?tenantId=1&queryField=id&searchTerm=a&startRecord=0&pageSize=5&orderBy=id&order=asc"
-  //     )
-  //     .reply(200, { data: {} });
+  test("SAAS - SEARCH Tables With Query Field Success", async () => {
+    mockApi
+      .onGet(
+        "testTable?tenantId=1&queryField=id&searchTerm=a&startRecord=0&pageSize=5&orderBy=id&order=asc"
+      )
+      .reply(200, { data: {} });
 
-  //   const result = await store.dispatch(
-  //     searchDataWithQueryField({
-  //       queryField: "id",
-  //       searchTerm: "a",
-  //       startRecord: "0",
-  //       pageSize: "5",
-  //       orderBy: "id",
-  //       order: "asc",
-  //       requestParams: { tenantId: "1", tableName: "testTable" },
-  //     })
-  //   );
+    const result = await store.dispatch(
+      searchDataWithQueryField({
+        queryField: "id",
+        searchTerm: "a",
+        startRecord: "0",
+        pageSize: "5",
+        orderBy: "id",
+        order: "asc",
+        requestParams: { tenantId: "1", tableName: "testTable" },
+      })
+    );
 
-  //   // console.log(result.type);
-  //   expect(result.type).toBe("searchDataWithQueryField/fulfilled");
-  // });
+    console.log(result.type);
+    expect(result.type).toBe("searchDataWithQueryField/fulfilled");
+  });
 
   test("SAAS - SEARCH Tables With Query Field Failure", async () => {
     mockApi
