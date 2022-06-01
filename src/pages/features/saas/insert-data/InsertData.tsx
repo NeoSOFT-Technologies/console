97dd10581e41171be129f34e41567b7a1c2aa87f
shubhamphepale <shubhamphepale@gmail.com>
2022-06-01 16:42:35 +0530

    validation for insert data

--- src/pages/features/saas/insert-data/InsertData.tsx
+++ src/pages/features/saas/insert-data/InsertData.tsx
@@ -65,6 +65,7 @@ export default function InputData() {
       } else {
         dispatch(inputTableDataWithoutNrt(initialState));
       }
+      setInputData("");
     } else {
       ToastAlert("Invalid Data", "error");
     }
