97c82fbbc080a98bcbe0d1df5dc397947731343b
gurpreet1101 <mailtogurpreet1991@gmail.com>
2022-06-08 22:55:09 +0530

    Changes for Edit Table screen

--- src/store/features/saas/manage-table/update-table-schema/slice.ts
+++ src/store/features/saas/manage-table/update-table-schema/slice.ts
@@ -1,7 +1,6 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import { updateTableSchemaService } from "../../../../../services/saas/api/api";
 import { IUpdateTable } from "../../../../../types/saas";
-import error from "../../../../../utils/error";
 
 interface IUpdateTableSchemaState {
   data?: string;
@@ -29,10 +28,12 @@ export const updateTableSchema = createAsyncThunk(
       // );
       return response.data;
     } catch (error_: any) {
-      // console.log(error_, "||", error(error_));
-      const errorMessage = error(error_);
-      // console.log(`Error : ` + JSON.stringify(error_));
-      throw new Error(errorMessage);
+      let errorMsg = "Undefined Error";
+      errorMsg =
+        error_.response.data !== undefined
+          ? error_.response.data.message
+          : error_.message;
+      throw new Error(errorMsg);
     }
   }
 );
@@ -53,11 +54,7 @@ const slice = createSlice({
     });
     builder.addCase(updateTableSchema.rejected, (state, action: any) => {
       state.loading = false;
-      const errorMessage = action.error.message.split(" ");
-      state.error = errorMessage[errorMessage.length - 1];
-      if (state.error === "403" || state.error === "401") {
-        alert("Invalid Token");
-      }
+      state.error = action.error.message;
     });
   },
 });
