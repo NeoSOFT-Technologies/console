bd3fe01f1bc8cb17f46bbade5ab57a293665aa4b
ojahagirdar <jahagirdaromkar@gmail.com>
2022-06-15 09:36:21 +0530

    icons Added

--- src/routes/saas/saas.ts
+++ src/routes/saas/saas.ts
@@ -7,22 +7,22 @@ const saasRoutes = [
   {
     path: "/saas/insert-data",
     title: "Insert Data",
-    icon: "bi bi-list-ul",
+    icon: "bi bi-clipboard-plus",
   },
   {
     path: "/saas/search-data",
     title: "Search Data",
-    icon: "bi bi-list-ul",
+    icon: "bi bi-search",
   },
   {
     path: "/saas/manage-table",
     title: "Manage Table",
-    icon: "bi bi-list-ul",
+    icon: "bi bi-table",
   },
   {
     path: "/saas/restore-table",
     title: "Restore Table",
-    icon: "bi bi-list-ul",
+    icon: "bi bi-bootstrap-reboot",
   },
 ];
 export default saasRoutes;
