8f785b7c59413a928d785259cd24c93364d283ed
alfaiz.khan <alfaiz.khan@neosoftmail.com>
2022-06-23 18:43:52 +0530

    added payload

---
+++ src/store/features/gateway/policy/create/duplicate-payload.ts
@@ -0,0 +1,46 @@
+import { IDPolicyCreateState } from "./duplicate-index";
+export const initialState: IDPolicyCreateState = {
+  data: {
+    form: {
+      Name: "",
+      Active: true,
+      KeysInactive: true,
+      MaxQuota: -1,
+      QuotaRate: -1,
+      Rate: 0,
+      Per: 0,
+      ThrottleInterval: -1,
+      ThrottleRetries: -1,
+      State: "active",
+      KeyExpiresIn: 0,
+      Tags: [],
+      APIs: [],
+      Partitions: {
+        quota: false,
+        rate_limit: false,
+        complexity: false,
+        acl: false,
+        per_api: true,
+      },
+    },
+    errors: {
+      Name: "",
+      Policies: "",
+      GlobalLimit: {
+        ApiId: "",
+        Per: "",
+        Rate: "",
+        Quota: "",
+        Expires: "",
+        QuotaRenewalRate: "",
+        ThrottleInterval: "",
+        ThrottleRetries: "",
+      },
+      PerApiLimit: [],
+    },
+  },
+  loading: false,
+  error: undefined,
+};
+
+export const emptyState: IDPolicyCreateState = { ...initialState };
