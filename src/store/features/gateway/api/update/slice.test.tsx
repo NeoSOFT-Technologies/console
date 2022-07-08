0e391c05f3735c09308ad689c431ee88decc1146
dwivedi-ankita <ankita.dwivedi@neosoftmail.com>
2022-07-08 10:07:49 +0530

    Added some unit test cases

--- src/store/features/gateway/api/update/slice.test.tsx
+++ src/store/features/gateway/api/update/slice.test.tsx
@@ -2,8 +2,128 @@ import mockApi from "../../../../../resources/tenant/testconfig";
 import store from "../../../../../store/index";
 import { updateApi } from "./slice";
 
+const response = {
+  ApiId: "b9eef321-8bb7-43d3-982a-59e8c9225ca5",
+  Name: "api1",
+  ListenPath: "/api1/",
+  StripListenPath: false,
+  TargetUrl: "https://httpbin.org",
+  SelectedTabIndex: "1",
+  IsActive: true,
+  IsInternal: false,
+  Protocol: "http",
+  RateLimit: {
+    Rate: 0,
+    Per: 0,
+    IsDisabled: false,
+  },
+  Blacklist: [],
+  Whitelist: [],
+  VersioningInfo: {
+    Location: 1,
+    Key: "",
+  },
+  IsVersioningDisabled: false,
+  DefaultVersion: "",
+  Versions2: [],
+  Versions: [
+    {
+      Name: "v1",
+      OverrideTarget: "https://httpbin.orgs",
+      Expires: "",
+      GlobalRequestHeaders: {},
+      GlobalRequestHeadersRemove: [],
+      GlobalResponseHeaders: {},
+      GlobalResponseHeadersRemove: [],
+      ExtendedPaths: undefined,
+    },
+  ],
+  AuthType: "standard",
+  EnableMTLS: false,
+  CertIds: [],
+  OpenidOptions: {
+    Providers: [],
+  },
+  EnableRoundRobin: false,
+  LoadBalancingTargets: [],
+  IsQuotaDisabled: false,
+  CORS: {
+    IsEnabled: false,
+    AllowedOrigins: [],
+    AllowedMethods: [],
+    AllowedHeaders: [],
+    ExposedHeaders: [],
+    AllowCredentials: false,
+    MaxAge: 0,
+    OptionsPassthrough: false,
+    Debug: false,
+  },
+};
+
 test("calling the state of update api", async () => {
-  mockApi.onPatch("/ApplicationGateway").reply(200, {});
+  mockApi.onPatch("/ApplicationGateway").reply(200, response);
+
+  await store.dispatch(
+    updateApi({
+      ApiId: "b9eef321-8bb7-43d3-982a-59e8c9225ca5",
+      Name: "api1",
+      ListenPath: "/api1/",
+      StripListenPath: false,
+      TargetUrl: "https://httpbin.org",
+      SelectedTabIndex: "1",
+      IsActive: true,
+      IsInternal: false,
+      Protocol: "http",
+      RateLimit: {
+        Rate: 0,
+        Per: 0,
+        IsDisabled: false,
+      },
+      Blacklist: [],
+      Whitelist: [],
+      VersioningInfo: {
+        Location: 1,
+        Key: "",
+      },
+      IsVersioningDisabled: false,
+      DefaultVersion: "",
+      Versions2: [],
+      Versions: [
+        {
+          Name: "v1",
+          OverrideTarget: "https://httpbin.orgs",
+          Expires: "",
+          GlobalRequestHeaders: {},
+          GlobalRequestHeadersRemove: [],
+          GlobalResponseHeaders: {},
+          GlobalResponseHeadersRemove: [],
+          ExtendedPaths: undefined,
+        },
+      ],
+      AuthType: "standard",
+      EnableMTLS: false,
+      CertIds: [],
+      OpenidOptions: {
+        Providers: [],
+      },
+      EnableRoundRobin: false,
+      LoadBalancingTargets: [],
+      IsQuotaDisabled: false,
+      CORS: {
+        IsEnabled: false,
+        AllowedOrigins: [],
+        AllowedMethods: [],
+        AllowedHeaders: [],
+        ExposedHeaders: [],
+        AllowCredentials: false,
+        MaxAge: 0,
+        OptionsPassthrough: false,
+        Debug: false,
+      },
+    })
+  );
+  // expect(result.type).toBe("api/update/fulfilled");
+
   //   const result = await store.dispatch(
   //     updateApi({
   //       ApiId: "f7764699-e83d-4971-aed3-3e508ac97d70",
@@ -66,11 +186,11 @@ test("calling the state of update api", async () => {
 });
 
 test("calling the state of update api", async () => {
-  mockApi.onDelete("/ApplicationGateway").reply(404, {});
+  mockApi.onPatch("/ApplicationGateway").reply(404, {});
 
   const result = await store.dispatch(
     updateApi({
-      ApiId: "0",
+      ApiId: "b9eef321-8bb7-43d3-982a-59e8c9225ca5",
       Name: "api1",
       ListenPath: "/api1/",
       StripListenPath: false,
