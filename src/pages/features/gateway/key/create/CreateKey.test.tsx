ec76346d49c2900ed8634cc3b88e4601ebad1823
avivekm <vivek.maurya@neosoftmail.com>
2022-07-12 17:28:56 +0530

    create key modifications

--- src/pages/features/gateway/key/create/CreateKey.test.tsx
+++ src/pages/features/gateway/key/create/CreateKey.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen, fireEvent } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import React from "react";
 import "@testing-library/jest-dom/extend-expect";
 import { Provider } from "react-redux";
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 import store from "../../../../../store";
 import CreateKey from "./CreateKey";
 
-it("Test render of CreateKey", () => {
+it("Test render of CreateKey", async () => {
   render(
     <BrowserRouter>
       <Provider store={store}>
@@ -15,27 +15,4 @@ it("Test render of CreateKey", () => {
     </BrowserRouter>
   );
   expect(screen).toBeDefined();
-});
-
-it("Test buttons present", () => {
-  render(
-    <BrowserRouter>
-      <Provider store={store}>
-        <CreateKey />
-      </Provider>
-    </BrowserRouter>
-  );
-  expect(screen).toBeDefined();
-
-  const submitBtn = screen.getByTestId("form-input");
-  expect(submitBtn).toBeInTheDocument();
-  fireEvent.submit(submitBtn);
-
-  const cancelBtn = screen.getByTestId("cancel-input");
-  expect(cancelBtn).toBeInTheDocument();
-  fireEvent.click(cancelBtn);
-
-  // const copyBtn = screen.getByTestId("copy-input");
-  // expect(copyBtn).toBeInTheDocument();
-  // fireEvent.click(copyBtn);
 });
