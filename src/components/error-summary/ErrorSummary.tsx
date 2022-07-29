a029e9ad312a520208c86a4d826088b322b15bcf
Himani-ja <himani.jain2309@gmail.com>
2022-07-29 11:54:30 +0530

    code smell reduce

--- src/components/error-summary/ErrorSummary.tsx
+++ src/components/error-summary/ErrorSummary.tsx
@@ -6,34 +6,52 @@ function generateBullets(key: any, value: any, check?: any, name?: any) {
       <div key={key}>
         <>
           <ul>
-            {name === undefined ? (
-              <li>
-                {key} : {value!}
-              </li>
-            ) : checkFlag ? (
-              <>
-                <li>
-                  <b>{name}</b>
+            {(() => {
+              if (name === undefined) {
+                return (
+                  <li>
+                    {key} : {value || ""}
+                  </li>
+                );
+              } else if (checkFlag) {
+                return (
+                  <>
+                    <li>
+                      <b>{name}</b>
+                      <ul>
+                        <li>
+                          {key} : {value}
+                        </li>
+                      </ul>
+                    </li>
+                  </>
+                );
+              } else {
+                return (
                   <ul>
                     <li>
                       {key} : {value}
                     </li>
                   </ul>
-                </li>
-              </>
-            ) : (
-              <ul>
-                <li>
-                  {key} : {value}
-                </li>
-              </ul>
-            )}
+                );
+              }
+            })()}
           </ul>
-          {typeof check === "boolean" ? (check = true) : (check = checkFlag)}
+          {(() => {
+            return typeof check === "boolean"
+              ? (check = true)
+              : (check = checkFlag);
+          })()}
         </>
       </div>
     </>
   ) : undefined;
+}
+function valuew(value: any, check: any) {
+  if (value !== "") {
+    check = true;
+  }
+  return check;
 }
 export const errorSummary = (errors: any, data?: any) => {
   let list1: any;
@@ -41,66 +59,58 @@ export const errorSummary = (errors: any, data?: any) => {
   let check2: any;
   let check3: any;
   let objCounter = 0;
-  const list: any = Object.entries(errors).map(([key, value], index) => {
+  const list: any = Object.entries(errors).map(([key, value]) => {
     if (typeof value === "string") {
-      if (value !== "") {
-        check1 = true;
-      }
+      check1 = valuew(value, check1);
       return generateBullets(key, value);
-    } else if (typeof value === "object" && !Array.isArray(value!)) {
-      list1 = Object.entries(value!).map(([key1, value1], index1) => {
-        if (value1 !== "") {
-          check2 = true;
-        }
+    } else if (typeof value === "object" && !Array.isArray(value || "")) {
+      list1 = Object.entries(value || "").map(([key1, value1]) => {
+        check2 = valuew(value1, check2);
         if (value1 !== "") {
           objCounter = objCounter + 1;
         }
         return generateBullets(key1, value1, objCounter, key);
       });
-    } else if (Array.isArray(value!)) {
+    } else if (Array.isArray(value)) {
+      // look
       list1 = Object.entries(value).map(([key1, value1], index1) => {
         if (typeof value1 === "object") {
           const { ApiId, ApiName, ...rest } = value1;
+          console.log(key1, ApiId);
           let checkEmpty = Object.values(value1).every(
             (x) => x === null || x === ""
           );
-          const list2: any = Object.entries(rest).map(
-            ([key2, value2], index2) => {
-              if (value2 !== "") {
-                check3 = true;
-              }
-
-              const a = generateBullets(key2, value2, checkEmpty, ApiName);
-              checkEmpty = true;
-              return a;
-            }
-          );
+          const list2: any = Object.entries(rest).map(([key2, value2]) => {
+            check3 = valuew(value2, check3);
+            const a = generateBullets(key2, value2, checkEmpty, ApiName);
+            checkEmpty = true;
+            return a;
+          });
 
           return list2;
         } else if (typeof value1 === "string") {
-          if (value1 !== "") {
-            check3 = true;
-          }
-
+          check3 = valuew(value1, check3);
           return check3 ? (
             <div key={index1}>
               <>
                 <ul>
                   <>
-                    {value1 !== "" && value1 !== undefined ? (
-                      <li>
-                        <>
-                          <b>{data[index1]}</b>
-                        </>
-                        <ul>
-                          <li>
-                            <>{value1}</>
-                          </li>
-                        </ul>
-                      </li>
-                    ) : (
-                      <></>
-                    )}
+                    {(() => {
+                      return value1 !== "" && value1 !== undefined ? (
+                        <li>
+                          <>
+                            <b>{data[index1]}</b>
+                          </>
+                          <ul>
+                            <li>
+                              <>{value1}</>
+                            </li>
+                          </ul>
+                        </li>
+                      ) : (
+                        <></>
+                      );
+                    })()}
                   </>
                 </ul>
               </>
