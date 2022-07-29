c268ec79701fef668bb059804a3263d482609771
dwivedi-ankita <ankita.dwivedi@neosoftmail.com>
2022-07-29 15:46:59 +0530

    api-constant code smell reduced

--- src/resources/gateway/api/api-constants.ts
+++ src/resources/gateway/api/api-constants.ts
@@ -9,21 +9,21 @@ export const regexForName = /^[ A-Za-z][\d A-Za-z]{3,29}$/;
 export const regexForListenPath = /^\/[A-Za-z][\dA-Za-z]*\/$/;
 
 export const regexForTargetUrl =
-  /^((https?|ftp|file):\/\/|www\.|ftp\.)(\([\w!#$%&+,./:=?@\\|]*\)|[\d!#$%&+,./:=?@A-Z\\_|-])*((\.[a-z0-9]{1,6}|:[a-z0-9]{1,6}))/i;
+  /^((https?|ftp|file):\/\/|www\.|ftp\.)(\([\w!#$%&+,./:=?@\\|]*\)|[\d!#$%&+,./:=?@A-Z\\_|-])*((\.[a-z0-9]|:[a-z0-9]))/i;
 
 export const regexForNumber = /^-?\d+$/;
 
 export const regexForOverrideTarget =
-  /^((https?|ftp|file):\/\/|www\.|ftp\.)(\([\w!#$%&+,./:=?@\\|]*\)|[\d!#$%&+,./:=?@A-Z\\_|-])*((\.[a-z0-9]{1,6}|:[a-z0-9]{1,6}))/i;
+  /^((https?|ftp|file):\/\/|www\.|ftp\.)(\([\w!#$%&+,./:=?@\\|]*\)|[\d!#$%&+,./:=?@A-Z\\_|-])*((\.[a-z0-9]|:[a-z0-9]))/i;
 
 export const regexForIssuer =
-  /^((https?|ftp|file):\/\/|www\.|ftp\.)(\([\w!#$%&+,./:=?@\\|]*\)|[\d!#$%&+,./:=?@A-Z\\_|-])*((\.[a-z0-9]{1,6}|:[a-z0-9]{1,6}))/i;
+  /^((https?|ftp|file):\/\/|www\.|ftp\.)(\([\w!#$%&+,./:=?@\\|]*\)|[\d!#$%&+,./:=?@A-Z\\_|-])*((\.[a-z0-9]|:[a-z0-9]))/i;
 
 export const regexForAllowedOrigins =
-  /^(((https?|ftp|file):\/\/|www\.|ftp\.)(\([\w!#$%&+,./:=?@\\|]*\)|[\d!#$%&+,./:=?@A-Z\\_|-])*((\.[a-z0-9]{1,6}|:[a-z0-9]{1,6}))|(\*))/i;
+  /^(((https?):\/\/)(\([\w!#$%&+,./:=?@\\|]*\)|[\d!#$%&+,./:=?@A-Z\\_|-])*((\.[a-z0-9]|:[a-z0-9]))|(\*))/i;
 
 export const regexForIPAddress =
-  /^(?:\b25[0-5]|\b2[0-4]\d|\b[01]?\d{1,2})(\.(25[0-5]|2[0-4]\d|[01]?\d{1,2})){3}$/;
+  /^(\b((25[0-5]|2[0-4]\d|[01]?\d\d?)(\.|$)){4}\b)/i;
 
 export function setFormData(e: any, dispatch: any, state: any) {
   const newState = setNestedState(e, state);
