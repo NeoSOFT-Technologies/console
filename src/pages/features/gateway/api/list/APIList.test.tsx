39780605c7356a925ca5d555cdf2b0de56e63188
avivekm <vivek.maurya@neosoftmail.com>
2022-08-09 10:50:04 +0530

    lists fixes

--- src/pages/features/gateway/api/list/APIList.test.tsx
+++ src/pages/features/gateway/api/list/APIList.test.tsx
@@ -1,37 +1,10 @@
 import { render, screen } from "@testing-library/react";
-import axios from "axios";
-import MockAdapter from "axios-mock-adapter";
 import React from "react";
 import "@testing-library/jest-dom/extend-expect";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import store from "../../../../../store/index";
 import APIList from "./APIList";
-
-const mockApi = new MockAdapter(axios);
-
-it("render without crashing API list", () => {
-  mockApi.onGet("/ApplicationGateway?pageNum=1&pageSize=1").reply(200, {
-    data: [
-      {
-        Name: "Api1",
-        CreatedDate: "2022-07-07T14:09:40.405677+00:00",
-        TargetUrl: "https://httpbin.org",
-        IsActive: true,
-        Id: "9dd100136a6a4e00af04bcece0eb1c8a",
-        AuthType: "standard",
-      },
-    ],
-    count: 1,
-  });
-  render(
-    <BrowserRouter>
-      <Provider store={store}>
-        <APIList />
-      </Provider>
-    </BrowserRouter>
-  );
-});
 
 it("check buttons and inputs", async () => {
   render(
--- src/pages/features/gateway/policy/list/PolicyList.test.tsx
+++ src/pages/features/gateway/policy/list/PolicyList.test.tsx
@@ -1,37 +1,10 @@
 import { render, screen } from "@testing-library/react";
-import axios from "axios";
-import MockAdapter from "axios-mock-adapter";
 import React from "react";
 import "@testing-library/jest-dom/extend-expect";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import store from "../../../../../store/index";
 import PolicyList from "./PolicyList";
-
-const mockApi = new MockAdapter(axios);
-
-it("render without crashing Policy list", () => {
-  mockApi.onGet("/Policy?pageNum=1&pageSize=1").reply(200, {
-    data: [
-      {
-        Action: "delete",
-        Id: "9dd100136a6a4e00af04bcece0eb1c8a",
-        Name: "policy1",
-        State: "state",
-        Apis: ["api1", "api2"],
-        AuthType: "standard",
-      },
-    ],
-    count: 1,
-  });
-  render(
-    <BrowserRouter>
-      <Provider store={store}>
-        <PolicyList />
-      </Provider>
-    </BrowserRouter>
-  );
-});
 
 it("check buttons and inputs", async () => {
   render(
